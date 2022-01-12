09771f962951ac06e060253e78bf7a9333284389
seyedsharifi12 <83226656+seyedsharifi12@users.noreply.github.com>
2022-01-12 18:43:03 +0000

    Server branch to be reviewed and merged. (#1)
    * Initialize postgres
    
    * feat: endpoints and static file created injson formatt
    
    * feat: updated json file added duration key

--- server/api.js
+++ server/api.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
+const energisers = require("./energiser.json");
+
 
 const router = new Router();
 
+
 router.get("/", (_, res) => {
 	res.json({ message: "Hello, world!" });
+});
+router.get("/energisers", (req, res) => {
+	res.json(energisers);
+});
+router.get("/energisers/:id", (req, res) => {
+	let energiserId = req.params.id;
+	const foundEnegiser = energisers.find((energiser) => energiser.id === energiserId);
+	res.json(foundEnegiser);
 });
 
 export default router;
