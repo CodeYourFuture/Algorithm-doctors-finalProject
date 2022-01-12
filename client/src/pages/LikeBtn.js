b1f53dda88a95751e6c0c3328482d5bab001acdf
konikalily <konika.l.biswas@gmail.com>
2022-01-12 20:25:29 +0000

    Like/dislike buttons component

---
+++ client/src/pages/LikeBtn.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { FaThumbsUp, FaThumbsDown } from "react-icons/fa";
+
+function LikeBtn({
+	existingLikeCount,
+	onLike,
+	existingDislikeCount,
+	onDislike,
+}) {
+	// let [likeCount, setLikeCount] = useState(0);
+	//let [dislikeCount, setDislikeCount] = useState(0);
+	let [userHasLiked, setUserHasLiked] = useState(false);
+	let [userHasDisliked, setUserHasDisliked] = useState(false);
+
+	// Create LikeCount event handler
+	const thumbsUp = () => {
+		if (!userHasLiked) {
+			onLike();
+		}
+		setUserHasLiked(true);
+	};
+	//Create DislikeCount event handler
+	const thumbsDown = () => {
+		if (!userHasDisliked) {
+			onDislike();
+		}
+		setUserHasDisliked(true);
+	};
+
+	return (
+		<div className="">
+			<div className="like_dislike_btn">
+				<button onClick={thumbsUp} className="btn">
+					<FaThumbsUp style={{ color: "red" }} />
+					{userHasLiked ? existingLikeCount + 1 : existingLikeCount}
+					{/* {LikeCount} */}
+				</button>
+				<button onClick={thumbsDown} className="btn">
+					<FaThumbsDown style={{ color: "gray" }} />
+					{userHasDisliked ? existingDislikeCount + 1 : existingDislikeCount}
+					{/* {dislikeCount} */}
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default LikeBtn;
+
+
+
+
+{
+	/* <LikeBtn
+	existingLikeCount={5}
+	onLike={() => console.log("like was clicked")}
+	existingDislikeCount={1}
+	onDislike={() => console.log("like was clicked")}
+/>; */
+}
