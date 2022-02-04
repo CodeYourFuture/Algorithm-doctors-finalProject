88716afe833a953cf754aa1a9fbcb8aa488e1579
Ash-97 <Ashai96@live.co.uk>
2022-02-04 19:30:09 +0000

    Added timer to energiser page.

--- client/src/pages/Components/EnergiserPage.js
+++ client/src/pages/Components/EnergiserPage.js
@@ -1,6 +1,7 @@
-import React , { useEffect, useState }  from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "../styles/EnergiserPage.css";
+import CountdownTimer from "./CountdownTimer";
 
 const EnergiserPage = () => {
 	const { id } = useParams();
@@ -23,22 +24,33 @@ const EnergiserPage = () => {
 	}, [id]);
 	return (
 		<main>
-			{energiser?<div>
-				<header className='energiser-page-title'>
-					<h1>Algorithm Doctors’ Energisers</h1>
-				</header>
-				<div className='energiser-page-container'>
-					<h2 className='energiser-page-name'>{energiser[0].name}</h2>
-					<p className='energiser-page-description'>{energiser[0].description}</p>
-					<div className='energiser-page-partduration'>
-						<p>Participants: {energiser[0].participants}</p>
-						<p>{energiser[0].duration} Mins</p>
+			{energiser ? (
+				<div>
+					<header className="energiser-page-title">
+						<h1>Algorithm Doctors’ Energisers</h1>
+					</header>
+					<div className="energiser-page-container">
+
+						<div className="timer">
+						<h2 className="energiser-page-name">{energiser[0].name}</h2>
+							<CountdownTimer duration={energiser[0].duration} />
+						</div>
+						<p className="energiser-page-description">
+							{energiser[0].description}
+						</p>
+						<div className="energiser-page-partduration">
+							<p>Participants: {energiser[0].participants}</p>
+							<p>Duration: {energiser[0].duration} Mins</p>
+						</div>
+						<p className="energiser-page-how">How To Run:</p>
+						<p className="energiser-page-duration">
+							{energiser[0].instructions}
+						</p>
 					</div>
-					<p className='energiser-page-how'>How To Run:</p>
-					<p className='energiser-page-duration'>{energiser[0].instructions}</p>
 				</div>
-				</div>:<div>...Loading</div>
-				}
+			) : (
+				<div>...Loading</div>
+			)}
 		</main>
 	);
 };
